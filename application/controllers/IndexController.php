054635b903d8b8c56c7c9be8d6183944e3d569ad
Josh Butts <josh@joshbutts.com>
2010-08-10 16:42:08 -0500

    add sample mongo actions

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,12 +5,51 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->_helper->viewRenderer->setNoRender();
     }
 
     public function indexAction()
     {
-        // action body
+        
+    }
+
+    public function mongoInsertAction()
+    {
+        $mongoString = "mongodb://localhost";
+        $m = new Mongo($mongoString);
+        $db = $m->testdb;
+
+        $collection = $db->people;
+
+
+        $mydoc = array();
+        $mydoc["name"] = 'Josh';
+        $mydoc["tags"] = array("awesome", "super", "great-guy");
+        $mydoc["email"] = "josh@joshbutts.com";
+
+        $collection->insert($mydoc);
+        
+    }
+
+    public function mongoQueryAction()
+    {
+        $mongoString = "mongodb://localhost";
+        $m = new Mongo($mongoString);
+        $db = $m->testdb;
+        $collection = $db->people;
+        $josh = $collection->find(array('tags' => 'great-guy'));
+        Zend_Debug::dump($josh->getNext());
+        Zend_Debug::dump($josh->count());
+    }
+
+    public function mongoDeleteAction()
+    {
+        $mongoString = "mongodb://localhost";
+        $m = new Mongo($mongoString);
+        $db = $m->testdb;
+        $collection = $db->people;
+        $josh = $collection->findOne(array('name' => 'Josh'));
+        $collection->remove(array("_id" => $josh["_id"]));
     }
 
 
